7a71f958d4338f353af5517d12ef57d5b64609ca
jhonduarte7 <johnduarte7@gmail.com>
2024-01-02 15:54:11 -0300

    cliente con sprint boot JAKARTA

---
+++ pay-system/spring-boot/apirest-paysystem/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into clientes (nombre, apellido, email, create_at) values ('Jhon', 'Duarte', 'johnduarte7@gmail.com', '2019-08-23');
+insert into clientes (nombre, apellido, email, create_at) values ('Samuel', 'Rojas', 'Samuel@gmail.com', '2019-08-23');
+insert into clientes (nombre, apellido, email, create_at) values ('Alejandro', 'Perez', 'Alejandro@gmail.com', '2019-08-23');
+insert into clientes (nombre, apellido, email, create_at) values ('Angela', 'Suarez', 'Angela@gmail.com', '2019-08-23');
+insert into clientes (nombre, apellido, email, create_at) values ('Elizabeth', 'Fernadez', 'Elizabeth@gmail.com', '2019-08-23');
